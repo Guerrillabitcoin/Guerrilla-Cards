61dd39a192fd667b4207768542a9b10a887548e7
GUERRILLA <115675632+Guerrillabitcoin@users.noreply.github.com>
2026-09-19 03:27:38 +0200

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useRouter } from 'expo-router';
+import { useLocalSearchParams, useRouter } from 'expo-router';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import {
   Alert,
